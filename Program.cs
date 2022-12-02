ab8118660f47dbbf94e2cb6c24ea93460c1ad825
Steve Ognibene <steve.ognibene@outlook.com>
2022-12-02 10:52:47 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using common;
+using day1;
+
+namespace advent_of_code_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var input = Utils.GetResourceStringFromAssembly<Day1>("day1.input.txt");
+                var day1 = new Day1(input.ReplaceLineEndings("\n").Split('\n'));
+                Console.WriteLine($"Day 1 answer 1: {day1.GetAnswerForPart1()}");
+                Console.WriteLine($"Day 1 answer 2: {day1.GetAnswerForPart2()}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# advent-of-code-2022
+ Steve Ognibene's answers to the Advent of Code 2022 challenges
