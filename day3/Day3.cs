21a156bb2615ac3b8c4e0f0fab02d237309aafa3
Steve Ognibene <steve.ognibene@outlook.com>
2022-12-04 20:37:11 -0500

    Day 3 complete

---
+++ day3/Day3.cs
@@ -0,0 +1,75 @@
+﻿using common;
+using System.ComponentModel;
+
+namespace day3
+{
+    public class Day3 : IAdventOfCodeDay
+    {
+        public Day3(IEnumerable<string> input)
+        {
+            Rucksacks = input
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Select(x => new Rucksack { Contents = x })
+                .ToList();
+        }
+
+        public List<Rucksack> Rucksacks { get; init; }
+
+        public string GetAnswerForPart1()
+        {
+            var result = 0;
+            foreach (var r in Rucksacks)
+            {
+                var itemsInFirst = r.FirstCompartment.ToHashSet();
+                var itemsInSecond = r.SecondCompartment.ToHashSet();
+                var itemsInBoth = itemsInFirst.Intersect(itemsInSecond).ToArray();
+                if (itemsInBoth.Length != 1) {
+                    throw new ApplicationException("This problem assumes exactly 1 item in both");
+                }
+                result += Priority(itemsInBoth.Single());
+            }
+            return result.ToString();
+        }
+
+        public string GetAnswerForPart2()
+        {
+            var result = 0;
+            for (var i = 0; i < Rucksacks.Count; i += 3)
+            {
+                var temp = Rucksacks[i].Contents.ToHashSet();
+                temp = temp.Intersect(Rucksacks[i + 1].Contents).ToHashSet();
+                temp = temp.Intersect(Rucksacks[i + 2].Contents).ToHashSet();
+
+                if (temp.Count != 1)
+                {
+                    throw new ApplicationException($"This problem assumes exactly 1 shared item in each set of three (i = {i}).");
+                }
+                result += Priority(temp.Single());
+            }
+            return result.ToString();
+        }
+
+        public static int Priority(char item)
+        {
+            const int LETTERS_IN_THE_ALPHABET = 26;
+            if (item >= 'a' && item <= 'z')
+            {
+                return item - 'a' + 1;
+            }
+            
+            if (item >= 'A' && item <= 'Z')
+            {
+                return item - 'A' + 1 + LETTERS_IN_THE_ALPHABET;
+            }
+
+            throw new NotSupportedException($"Item '{item}' not supported.  Only A-Z or a-z.");
+        }
+    }
+
+    public record Rucksack
+    {
+        public required string Contents { get; init; }
+        public string FirstCompartment => Contents.Substring(0, Contents.Length / 2);
+        public string SecondCompartment => Contents.Substring(Contents.Length / 2);
+    }
+}
---
+++ tests/Day3Tests.cs
@@ -0,0 +1,80 @@
+﻿using common;
+using day2;
+using day3;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace tests
+{
+    public class Day3Tests
+    {
+        [Fact]
+        public void CompartmentsAreIdentifiedCorrectly()
+        {
+            var sut = new Day3(example_input_1.Split('\n'));
+
+            sut.Rucksacks.Count.Should().Be(6);
+
+            sut.Rucksacks[0].Contents.Should().Be("vJrwpWtwJgWrhcsFMMfFFhFp");
+            sut.Rucksacks[0].FirstCompartment.Should().Be("vJrwpWtwJgWr");
+            sut.Rucksacks[0].SecondCompartment.Should().Be("hcsFMMfFFhFp");
+
+            sut.Rucksacks[1].FirstCompartment.Should().Be("jqHRNqRjqzjGDLGL");
+            sut.Rucksacks[1].SecondCompartment.Should().Be("rsFMfFZSrLrFZsSL");
+        }
+
+        [Theory]
+        [InlineData('a', 1)]
+        [InlineData('z', 26)]
+        [InlineData('A', 27)]
+        [InlineData('Z', 52)]
+        public void PrioritiesWorkAsDocumented(char item, int expectedPriority)
+        {
+            Day3.Priority(item).Should().Be(expectedPriority);
+        }
+
+        [Fact]
+        public void Part1WithSampleInputProducesDocumentedResult()
+        {
+            var sut = new Day3(example_input_1.Split('\n'));
+            sut.GetAnswerForPart1().Should().Be("157");
+        }
+
+        [Fact]
+        public void Part1WithActualInputProducesCorrectResult()
+        {
+            var input = Utils.GetResourceStringFromAssembly<Day3>("day3.input.txt");
+            var sut = new Day3(input.ReplaceLineEndings("\n").Split('\n'));
+            sut.GetAnswerForPart1().Should().Be("7737");
+        }
+
+        [Fact]
+        public void Part2WithSampleInputProducesDocumentedResult()
+        {
+            var sut = new Day3(example_input_1.Split('\n'));
+            sut.GetAnswerForPart2().Should().Be("70");
+        }
+
+        [Fact]
+        public void Part2WithActualInputProducesCorrectResult()
+        {
+            var input = Utils.GetResourceStringFromAssembly<Day3>("day3.input.txt");
+            var sut = new Day3(input.ReplaceLineEndings("\n").Split('\n'));
+            sut.GetAnswerForPart2().Should().Be("2697");
+        }
+
+        public static readonly string example_input_1 = """
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+            """.ReplaceLineEndings("\n");
+    }
+}
