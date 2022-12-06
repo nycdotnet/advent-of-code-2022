cc8217d96db4a9ebc51abf4a898571b8a7e113e0
Steve Ognibene <steve.ognibene@outlook.com>
2022-12-06 08:58:49 -0500

    working part 1

---
+++ day6/Day6.cs
@@ -0,0 +1,42 @@
+﻿using common;
+
+namespace day6
+{
+    public class Day6 : IAdventOfCodeDay
+    {
+        public string DataStreamBuffer { get; }
+
+        public Day6(IEnumerable<string> input)
+        {
+            DataStreamBuffer = input.Where(s => !string.IsNullOrEmpty(s)).Single();
+        }
+
+        public const int LENGTH_OF_START_OF_PACKET_MARKER = 4;
+
+        public int StartOfPacketMarkerIndex()
+        {
+            var buffer = DataStreamBuffer.AsSpan();
+            for (var i = 0; i < DataStreamBuffer.Length - LENGTH_OF_START_OF_PACKET_MARKER; i++)
+            {
+                if (buffer[i + 0] == buffer[i + 1] || buffer[i + 0] == buffer[i + 2] || buffer[i + 0] == buffer[i + 3] ||
+                    buffer[i + 1] == buffer[i + 2] || buffer[i + 1] == buffer[i + 3] ||
+                    buffer[i + 2] == buffer[i + 3])
+                {
+                    continue;
+                }
+                return i + LENGTH_OF_START_OF_PACKET_MARKER;
+            }
+            return -1;
+        }
+
+        public string GetAnswerForPart1()
+        {
+            return StartOfPacketMarkerIndex().ToString();
+        }
+
+        public string GetAnswerForPart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Day6Tests.cs
@@ -0,0 +1,35 @@
+﻿using common;
+using day5;
+using day6;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day6Tests
+    {
+        [Theory]
+        [MemberData(nameof(BuffersWithStart))]
+        public void StartOfPacketWorksAsExpected(string buffer, int expectedIndex)
+        {
+            var sut = new Day6(new string[] { buffer });
+            sut.StartOfPacketMarkerIndex().Should().Be(expectedIndex);
+        }
+
+        [Fact]
+        public void Part1WithActualInputProducesCorrectResult()
+        {
+            var input = Utils.GetResourceStringFromAssembly<Day6>("day6.input.txt");
+            var sut = new Day6(input.ReplaceLineEndings("\n").Split('\n'));
+            sut.GetAnswerForPart1().Should().Be("1909");
+        }
+
+        public static readonly TheoryData<string, int> BuffersWithStart = new() {
+            { "mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7 },
+            { "bvwbjplbgvbhsrlpgdmjqwftvncz", 5 },
+            { "nppdvjthqldpwncqszvftbrmjlhg", 6 },
+            { "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10 },
+            { "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11 }
+        };
+    }
+}
