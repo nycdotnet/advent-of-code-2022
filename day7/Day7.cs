ee9a21cc1384cb1d0a224cfa880889d17c936e47
Steve Ognibene <steve.ognibene@outlook.com>
2022-12-07 14:32:47 -0500

    Day 7 part 1 complete

---
+++ day7/Day7.cs
@@ -0,0 +1,212 @@
+﻿using common;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace day7
+{
+    public class Day7 : IAdventOfCodeDay
+    {
+        public Day7(IEnumerable<string> input)
+        {
+            RootElfFolder = ElfFolder.Parse(input);
+        }
+
+        public ElfFolder RootElfFolder { get; }
+
+        public string GetAnswerForPart1()
+        {
+            var atMost100k = RootElfFolder
+                .AllSubFolders()
+                .Select(f => (folder: f, totalSize: f.GetTotalSize()))
+                .Where(f => f.totalSize <= 100_000);
+
+            return atMost100k.Sum(x => x.totalSize).ToString();
+        }
+
+        public string GetAnswerForPart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface FileSystemEntry
+    {
+        string Name { get; }
+    }
+
+    public record ElfFile : FileSystemEntry
+    {
+        public required int Size { get; init; }
+        public required string Name { get; init; }
+    }
+
+    public class ElfFolder : FileSystemEntry
+    {
+        public ElfFolder(ElfFolder Parent, string Name)
+        {
+            this.Parent = Parent;
+            this.Name = Name;
+        }
+
+        public ElfFolder()
+        {
+            Parent = null;
+            Name = "/";
+        }
+
+        public string Name { get; private set; }
+        public ElfFolder? Parent { get; }
+
+        public int GetTotalSize() => Entries.Sum(e => e switch {
+                ElfFolder fo => fo.GetTotalSize(),
+                ElfFile f => f.Size,
+                _ => throw new UnreachableException()
+            });
+
+        public IEnumerable<ElfFolder> AllSubFolders()
+        {
+            for (var i = 0; i < Entries.Count; i++)
+            {
+                if (Entries[i] is ElfFolder folder)
+                {
+                    yield return folder;
+                    foreach (var f in folder.AllSubFolders())
+                    {
+                        yield return f;
+                    }
+                }
+            }
+        }
+
+        public List<FileSystemEntry> Entries { get; private set; } = new();
+        public static ElfFolder Parse(IEnumerable<string> input)
+        {
+            var content = input.Where(line => !string.IsNullOrEmpty(line)).ToArray();
+            var contentIndex = 0;
+
+            var root = new ElfFolder();
+            var currentFolder = root;
+            while (!DoneParsing())
+            {
+                var command = content[contentIndex];
+                if (command[0] != '$')
+                {
+                    throw new InvalidDataException($"Expected {command} to be a command.");
+                }
+
+                if (IsChangeDirectory(command))
+                {
+                    var targetFolderName = command.Substring(5);
+                    if (targetFolderName == "/")
+                    {
+                        currentFolder = root;
+                    }
+                    else if (targetFolderName == "..")
+                    {
+                        currentFolder = currentFolder.Parent ?? throw new NotSupportedException($"Can't issue cd .. when at root.");
+                    }
+                    else
+                    {
+                        var targetFolder = (ElfFolder?)currentFolder.Entries.Find(f => f is ElfFolder folder && folder.Name == targetFolderName);
+                        if (targetFolder != null)
+                        {
+                            currentFolder = targetFolder;
+                        }
+                        else
+                        {
+                            throw new KeyNotFoundException($"This folder does not contain subfolder {targetFolderName}.");
+                        }
+                    }
+                    contentIndex++;
+                    continue;
+                }
+                else if (IsListCommand(command))
+                {
+                    // we now enumerate everything in this directory
+                    contentIndex++;
+                    if (DoneParsing())
+                    {
+                        // there was nothing in this directory and it is the end of the input.
+                        continue;
+                    }
+                    var item = content[contentIndex];
+
+                    while (!IsCommand(item) && !DoneParsing())
+                    {
+                        var file = FileRegex.Match(item);
+                        if (file.Success)
+                        {
+                            currentFolder!.AddFile(new ElfFile
+                            {
+                                Name = file.Groups["FileName"].Value,
+                                Size = int.Parse(file.Groups["Size"].ValueSpan)
+                            });
+                        }
+                        else
+                        {
+                            if (!IsDirectory(item))
+                            {
+                                throw new UnreachableException();
+                            }
+
+                            currentFolder!.AddFolder(item.Substring(4));
+                        }
+
+                        contentIndex++;
+                        if (DoneParsing())
+                        {
+                            continue;
+                        }
+                        item = content[contentIndex];
+                    }
+                    continue;
+                }
+                else
+                {
+                    throw new NotSupportedException($"Command {command} not supported.");
+                }
+            }
+            return root;
+
+            bool DoneParsing() => contentIndex >= content.Length;
+        }
+
+        private void AddFolder(string name)
+        {
+            Entries.Add(new ElfFolder(this, name));
+        }
+
+        private void AddFile(ElfFile elfFile)
+        {
+            Entries.Add(elfFile);
+        }
+
+        public void WriteToStringBuilder(StringBuilder sb, int level = 0)
+        {
+            sb.AppendLine($"{ new string(' ', level * 2) }- {Name} (dir)");
+
+            foreach (var e in Entries)
+            {
+                if (e is ElfFolder folder)
+                {
+                    folder.WriteToStringBuilder(sb, level + 1);
+                }
+                else if (e is ElfFile file) {
+                    sb.AppendLine($"{new string(' ', (level + 1) * 2)}- {file.Name} (file, size={file.Size})");
+                }
+                else
+                {
+                    throw new UnreachableException();
+                }
+            }
+        }
+
+        private static bool IsChangeDirectory(string s) => s.StartsWith("$ cd ");
+        private static bool IsListCommand(string s) => s == "$ ls";
+        private static bool IsCommand(string s) => s.StartsWith('$');
+        private static bool IsDirectory(string s) => s.StartsWith("dir ");
+        private static bool IsFile(string s) => s.StartsWith("dir ");
+        public static Regex FileRegex = new Regex(@"^(?<Size>\d*) (?<FileName>.*)$");
+    }
+}
---
+++ tests/Day7Tests.cs
@@ -0,0 +1,97 @@
+﻿using common;
+using day5;
+using day7;
+using FluentAssertions;
+using System.Text;
+using Xunit;
+
+namespace tests
+{
+    public class Day7Tests
+    {
+        [Fact]
+        public void ParsingWorksAsDocumented()
+        {
+            var sut = new Day7(example_input_1.ReplaceLineEndings("\n").Split('\n'));
+            var sb = new StringBuilder();
+            sut.RootElfFolder.WriteToStringBuilder(sb);
+            sb.ToString().TrimEnd().Should().Be(example_directory_listing_1);
+        }
+
+        [Fact]
+        public void AllSubFoldersProducesExpectedResult()
+        {
+            var sut = new Day7(example_input_1.ReplaceLineEndings("\n").Split('\n'));
+            var result = sut.RootElfFolder.AllSubFolders().ToArray();
+            result.Select(f => f.Name).Should().Equal(new[] { "a", "e", "d" });
+        }
+
+        [Theory]
+        [InlineData("14848514 b.txt", 14848514, "b.txt")]
+        [InlineData("1234 file name with some spaces", 1234, "file name with some spaces")]
+        public void FileRegexWorksAsExpected(string fileSpec, int expectedSize, string expectedFileName)
+        {
+            var sut = ElfFolder.FileRegex.Match(fileSpec);
+            int.Parse(sut.Groups["Size"].ValueSpan).Should().Be(expectedSize);
+            sut.Groups["FileName"].Value.Should().Be(expectedFileName);
+        }
+
+        [Fact]
+        public void Part1WithSampleInputProducesDocumentedResult()
+        {
+            var sut = new Day7(example_input_1.ReplaceLineEndings("\n").Split('\n'));
+            sut.GetAnswerForPart1().Should().Be("95437");
+        }
+
+        [Fact]
+        public void Part1WithActualInputProducesCorrectResult()
+        {
+            var input = Utils.GetResourceStringFromAssembly<Day7>("day7.input.txt");
+            var sut = new Day7(input.ReplaceLineEndings("\n").Split('\n'));
+            sut.GetAnswerForPart1().Should().Be("1648397");
+        }
+
+        public static readonly string example_input_1 = """
+            $ cd /
+            $ ls
+            dir a
+            14848514 b.txt
+            8504156 c.dat
+            dir d
+            $ cd a
+            $ ls
+            dir e
+            29116 f
+            2557 g
+            62596 h.lst
+            $ cd e
+            $ ls
+            584 i
+            $ cd ..
+            $ cd ..
+            $ cd d
+            $ ls
+            4060174 j
+            8033020 d.log
+            5626152 d.ext
+            7214296 k
+            """.ReplaceLineEndings("\n");
+
+        public static readonly string example_directory_listing_1 = """
+            - / (dir)
+              - a (dir)
+                - e (dir)
+                  - i (file, size=584)
+                - f (file, size=29116)
+                - g (file, size=2557)
+                - h.lst (file, size=62596)
+              - b.txt (file, size=14848514)
+              - c.dat (file, size=8504156)
+              - d (dir)
+                - j (file, size=4060174)
+                - d.log (file, size=8033020)
+                - d.ext (file, size=5626152)
+                - k (file, size=7214296)
+            """;
+    }
+}
