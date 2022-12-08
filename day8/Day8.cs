df8f3eb6814bd70aae79d5585f127e5e85f76173
Steve Ognibene <steve.ognibene@outlook.com>
2022-12-08 09:22:30 -0500

    start day 8

---
+++ day8/Day8.cs
@@ -0,0 +1,24 @@
+﻿using common;
+
+namespace day8
+{
+    public class Day8 : IAdventOfCodeDay
+    {
+        public Day8(IEnumerable<string> input)
+        {
+            Data = input.Select(x => x.ToCharArray()).ToArray();
+        }
+
+        public char[][] Data { get; set; }
+
+        public string GetAnswerForPart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAnswerForPart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Day8Tests.cs
@@ -0,0 +1,28 @@
+﻿using day8;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day8Tests
+    {
+        [Fact]
+        public void ParsingWorksWithExampleData()
+        {
+            var sut = new Day8(example_input_1.ReplaceLineEndings("\n").Split('\n'));
+            sut.Data.Length.Should().Be(5);
+            sut.Data.Select(x => x.Length).Should().AllBeEquivalentTo(5);
+
+            sut.Data[0][0].Should().Be('3');
+            sut.Data[4][4].Should().Be('0');
+        }
+
+        public static readonly string example_input_1 = """
+            30373
+            25512
+            65332
+            33549
+            35390
+            """.ReplaceLineEndings("\n");
+    }
+}
