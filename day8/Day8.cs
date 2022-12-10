3eedf59c84cc4d0fda66d0be491880a03ec06eeb
Steve Ognibene <steve.ognibene@outlook.com>
2022-12-09 22:16:39 -0500

    Day 8 part 1 working

--- day8/Day8.cs
+++ day8/Day8.cs
@@ -1,4 +1,5 @@
 ﻿using common;
+using System.Text;
 
 namespace day8
 {
@@ -6,19 +7,187 @@ namespace day8
     {
         public Day8(IEnumerable<string> input)
         {
-            Data = input.Select(x => x.ToCharArray()).ToArray();
+            Data = input.Select(x => x.Select(CharDigitToInt).ToArray()).ToArray();
         }
 
-        public char[][] Data { get; set; }
+        public int[][] Data { get; set; }
 
         public string GetAnswerForPart1()
         {
-            throw new NotImplementedException();
+            var visibility = GetEmptyVisibilityMatrix();
+            MarkExtemitiesVisible(visibility);
+            MarkVisibleFromWest(visibility);
+            MarkVisibleFromEast(visibility);
+            MarkVisibleFromNorth(visibility);
+            MarkVisibleFromSouth(visibility);
+
+            return CountVisible(visibility).ToString();
+        }
+
+        private static int CountVisible(Visibility[][] visibility)
+        {
+            var count = 0;
+            for (var r = 0; r < visibility.Length; r++)
+            {
+                for (var c = 0; c < visibility[r].Length; c++)
+                {
+                    if (visibility[r][c] == Visibility.Visible)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
         }
 
         public string GetAnswerForPart2()
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Returns a visibility matrix for the size of the Data.
+        /// </summary>
+        /// <returns></returns>
+        public Visibility[][] GetEmptyVisibilityMatrix()
+        {
+            var result = new Visibility[Data.Length][];
+            for (var i = 0; i < Data.Length; i++)
+            {
+                result[i] = new Visibility[Data[i].Length];
+            }
+
+            return result;
+        }
+
+        public static string FormatVisibility(Visibility[][] visibility)
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < visibility.Length; i++)
+            {
+                sb.Append(string.Join("", visibility[i].Select(x => ((int)x).ToString())));
+                if (i + 1 < visibility.Length)
+                {
+                    sb.Append('\n');
+                }
+            }
+            return sb.ToString();
+        }
+
+        public void MarkVisibleFromWest(Visibility[][] visibility)
+        {
+            for (var rowIndex = 1; rowIndex < Data.Length - 1; rowIndex++)
+            {
+                var rowData = Data[rowIndex];
+                var highest = rowData[0];
+                for (var columnIndex = 1; columnIndex < rowData.Length - 1; columnIndex++)
+                {
+                    if (rowData[columnIndex] > highest)
+                    {
+                        visibility[rowIndex][columnIndex] = Visibility.Visible;
+                        highest = rowData[columnIndex];
+                        if (highest == MAX_HEIGHT)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void MarkVisibleFromEast(Visibility[][] visibility)
+        {
+            for (var rowIndex = 1; rowIndex < Data.Length - 1; rowIndex++)
+            {
+                var rowData = Data[rowIndex];
+                var highest = rowData[rowData.Length - 1];
+                for (var columnIndex = rowData.Length - 2; columnIndex > 0; columnIndex--)
+                {
+                    if (rowData[columnIndex] > highest)
+                    {
+                        visibility[rowIndex][columnIndex] = Visibility.Visible;
+                        highest = rowData[columnIndex];
+                        if (highest == MAX_HEIGHT)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void MarkVisibleFromNorth(Visibility[][] visibility)
+        {
+            for (var columnIndex = 1; columnIndex < Data[0].Length - 1; columnIndex++)
+            {
+                var highest = Data[0][columnIndex];
+                for (var rowIndex = 1; rowIndex < Data.Length - 1; rowIndex++)
+                {
+                    if (Data[rowIndex][columnIndex] > highest)
+                    {
+                        visibility[rowIndex][columnIndex] = Visibility.Visible;
+                        highest = Data[rowIndex][columnIndex];
+                        if (highest == MAX_HEIGHT)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void MarkVisibleFromSouth(Visibility[][] visibility)
+        {
+            for (var columnIndex = 1; columnIndex < Data[0].Length - 1; columnIndex++)
+            {
+                var highest = Data[Data.Length - 1][columnIndex];
+                for (var rowIndex = Data.Length - 2; rowIndex > 0; rowIndex--)
+                {
+                    if (Data[rowIndex][columnIndex] > highest)
+                    {
+                        visibility[rowIndex][columnIndex] = Visibility.Visible;
+                        highest = Data[rowIndex][columnIndex];
+                        if (highest == MAX_HEIGHT)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void MarkExtemitiesVisible(Visibility[][] matrix)
+        {
+            for(var i = 0; i < matrix[0].Length; i++)
+            {
+                matrix[0][i] = Visibility.Visible;
+            }
+
+            for (var i = 0; i < matrix[matrix.Length - 1].Length; i++)
+            {
+                matrix[matrix.Length - 1][i] = Visibility.Visible;
+            }
+
+            for (var i = 1; i < matrix.Length - 1; i++)
+            {
+                matrix[i][0] = Visibility.Visible;
+                matrix[i][matrix[i].Length - 1] = Visibility.Visible;
+            }
+        }
+
+        /// <summary>
+        /// Converts a character digit '0' through '9' to an int.  Does not
+        /// perform any validation.
+        /// </summary>
+        public static int CharDigitToInt(char c) => c - '0';
+
+        public enum Visibility
+        {
+            NotSure,
+            Visible,
+            Invisible
+        }
+
+        const int MAX_HEIGHT = 9;
     }
 }
--- tests/Day8Tests.cs
+++ tests/Day8Tests.cs
@@ -1,4 +1,5 @@
-﻿using day8;
+﻿using common;
+using day8;
 using FluentAssertions;
 using Xunit;
 
@@ -13,8 +14,103 @@ namespace tests
             sut.Data.Length.Should().Be(5);
             sut.Data.Select(x => x.Length).Should().AllBeEquivalentTo(5);
 
-            sut.Data[0][0].Should().Be('3');
-            sut.Data[4][4].Should().Be('0');
+            sut.Data[0][0].Should().Be(3);
+            sut.Data[4][4].Should().Be(0);
+        }
+
+        [Fact]
+        public void InitialVisibilityShowsAllEdgesVisible()
+        {
+            var sut = new Day8(example_input_1.ReplaceLineEndings("\n").Split('\n'));
+            var visibility = sut.GetEmptyVisibilityMatrix();
+            Day8.MarkExtemitiesVisible(visibility);
+
+            visibility.First().Should().AllBeEquivalentTo(Day8.Visibility.Visible);
+            visibility.Last().Should().AllBeEquivalentTo(Day8.Visibility.Visible);
+            
+            for (var i = 1; i < visibility.Length - 2; i++)
+            {
+                visibility[i][0].Should().Be(Day8.Visibility.Visible);
+                visibility[i][1].Should().Be(Day8.Visibility.NotSure);
+                visibility[i][2].Should().Be(Day8.Visibility.NotSure);
+                visibility[i][3].Should().Be(Day8.Visibility.NotSure);
+                visibility[i][4].Should().Be(Day8.Visibility.Visible);
+            }
+        }
+
+        [Fact]
+        public void MarkVisibleFromWestWorksAsExpected()
+        {
+            var sut = new Day8(interestingPyramid.Split('\n'));
+            var vis = sut.GetEmptyVisibilityMatrix();
+            sut.MarkVisibleFromWest(vis);
+            Day8.FormatVisibility(vis).Should().Be("""
+                00000
+                01110
+                01100
+                01000
+                00000
+                """.ReplaceLineEndings("\n"));
+        }
+
+        [Fact]
+        public void MarkVisibleFromNorthWorksAsExpected()
+        {
+            var sut = new Day8(interestingPyramid.Split('\n'));
+            var vis = sut.GetEmptyVisibilityMatrix();
+            sut.MarkVisibleFromWest(vis);
+            Day8.FormatVisibility(vis).Should().Be("""
+                00000
+                01110
+                01100
+                01000
+                00000
+                """.ReplaceLineEndings("\n"));
+        }
+
+        [Fact]
+        public void MarkVisibleFromEastWorksAsExpected()
+        {
+            var sut = new Day8(interestingPyramid.Split('\n'));
+            var vis = sut.GetEmptyVisibilityMatrix();
+            sut.MarkVisibleFromEast(vis);
+            Day8.FormatVisibility(vis).Should().Be("""
+                00000
+                00010
+                00110
+                01110
+                00000
+                """.ReplaceLineEndings("\n"));
+        }
+
+        [Fact]
+        public void MarkVisibleFromSouthWorksAsExpected()
+        {
+            var sut = new Day8(interestingPyramid.Split('\n'));
+            var vis = sut.GetEmptyVisibilityMatrix();
+            sut.MarkVisibleFromEast(vis);
+            Day8.FormatVisibility(vis).Should().Be("""
+                00000
+                00010
+                00110
+                01110
+                00000
+                """.ReplaceLineEndings("\n"));
+        }
+
+        [Fact]
+        public void Part1WithSampleInputProducesDocumentedResult()
+        {
+            var sut = new Day8(example_input_1.ReplaceLineEndings("\n").Split('\n'));
+            sut.GetAnswerForPart1().Should().Be("21");
+        }
+
+        [Fact]
+        public void Part1WithActualInputProducesCorrectResult()
+        {
+            var input = Utils.GetResourceStringFromAssembly<Day8>("day8.input.txt");
+            var sut = new Day8(input.ReplaceLineEndings("\n").Split('\n'));
+            sut.GetAnswerForPart1().Should().Be("1832");
         }
 
         public static readonly string example_input_1 = """
@@ -23,6 +119,22 @@ namespace tests
             65332
             33549
             35390
+            """.ReplaceLineEndings("\n");
+
+        public static readonly string interestingPyramid = """
+            00000
+            01230
+            02920
+            03210
+            00000
+            """.ReplaceLineEndings("\n");
+
+        public static readonly string expectedVisibility1 = """
+            11111
+            11121
+            11211
+            12121
+            11111
             """.ReplaceLineEndings("\n");
     }
 }
