8bb8f461463f5ef679d22b1f5db3ff5e40b1e6d9
Steve Ognibene <steve.ognibene@outlook.com>
2022-12-11 21:12:58 -0500

    New Linq SelectPartition extension method

---
+++ common/LinqExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace common
+{
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// Partitions an <see cref="IEnumerable{T}"/> on elements which when processed with
+        /// <paramref name="splitOn"/> return true.  Does not include those elements.
+        /// </summary>
+        public static IEnumerable<List<T>> SelectPartition<T>(
+            this IEnumerable<T> source,
+            Func<T, bool> splitOn)
+        {
+            ArgumentNullException.ThrowIfNull(source, nameof(source));
+            ArgumentNullException.ThrowIfNull(splitOn, nameof(splitOn));
+
+            bool shouldYield = false;
+            var buffer = new List<T>();
+            foreach (var item in source)
+            {
+                if (splitOn(item))
+                {
+                    yield return buffer;
+                    buffer = new List<T>();
+                }
+                else
+                {
+                    shouldYield = true;
+                    buffer.Add(item);
+                }
+            }
+            if (shouldYield)
+            {
+                yield return buffer;
+            }
+        }
+    }
+}
---
+++ tests/LinqExtensionsTests.cs
@@ -0,0 +1,91 @@
+﻿using common;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class LinqExtensionsTests
+    {
+        [Fact]
+        public void SelectPartitionWithNoPartitionsReturnsSingleItem()
+        {
+            var items = new string[] { "a", "b", "c" };
+            var result = items.SelectPartition(string.IsNullOrEmpty);
+
+            result.Single()[0].Should().Be("a");
+            result.Single()[1].Should().Be("b");
+            result.Single()[2].Should().Be("c");
+        }
+
+        [Fact]
+        public void SelectPartitionWithOnePartitionReturnsTwoLists()
+        {
+            var items = new string[] { "a", "b", "", "c", "d" };
+            var result = items.SelectPartition(string.IsNullOrEmpty).ToArray();
+
+            result.Should().HaveCount(2);
+            result[0][0].Should().Be("a");
+            result[0][1].Should().Be("b");
+            result[1][0].Should().Be("c");
+            result[1][1].Should().Be("d");
+        }
+
+        [Fact]
+        public void SelectPartitionThatEndsWithPartitionReturnsEmptyAtEnd()
+        {
+            var items = new string[] { "a", "b", "", "c", "d", "" };
+            var result = items.SelectPartition(string.IsNullOrEmpty).ToArray();
+
+            result.Should().HaveCount(3);
+            result[0][0].Should().Be("a");
+            result[0][1].Should().Be("b");
+            result[1][0].Should().Be("c");
+            result[1][1].Should().Be("d");
+            result[2].Should().BeEmpty();
+        }
+
+        [Fact]
+        public void SelectPartitionThatStartsWithPartitionReturnsEmptyAtStart()
+        {
+            var items = new string[] { "", "a", "b", "", "c", "d"};
+            var result = items.SelectPartition(string.IsNullOrEmpty).ToArray();
+
+            result.Should().HaveCount(3);
+            result[0].Should().BeEmpty();
+            result[1][0].Should().Be("a");
+            result[1][1].Should().Be("b");
+            result[2][0].Should().Be("c");
+            result[2][1].Should().Be("d");
+        }
+
+        [Fact]
+        public void SelectPartitionWithEmptyIsEmpty()
+        {
+            var items = new string[] { };
+            var result = items.SelectPartition(string.IsNullOrEmpty).ToArray();
+
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void SelectPartitionWithSinglePartitionHasSingleEmpty()
+        {
+            var items = new string[] { "" };
+            var result = items.SelectPartition(string.IsNullOrEmpty).ToArray();
+
+            result.Should().ContainSingle();
+            result[0].Should().BeEmpty();
+        }
+
+        [Fact]
+        public void SelectPartitionWithTwoPartitionsHasTwoEmpty()
+        {
+            var items = new string[] { "", "" };
+            var result = items.SelectPartition(string.IsNullOrEmpty).ToArray();
+
+            result.Should().HaveCount(2);
+            result[0].Should().BeEmpty();
+            result[1].Should().BeEmpty();
+        }
+    }
+}
