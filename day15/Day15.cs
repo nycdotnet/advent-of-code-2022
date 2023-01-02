5007ce152db1aa9bf0e328e9ca38d591f6d314d2
Steve Ognibene <steve.ognibene@outlook.com>
2023-01-02 14:55:52 -0500

    Day 15 working parsing and new 2d grid class

---
+++ day15/Day15.cs
@@ -0,0 +1,156 @@
+﻿using common;
+using System.ComponentModel;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace day15
+{
+    public class Day15 : IAdventOfCodeDay
+    {
+        public Day15(IEnumerable<string> input)
+        {
+            Sensors = input.Select(Sensor.MaybeParse).Where(s => s != null).ToList()!;
+            Grid = new TwoDimensionalGrid<char, int>('.', (_, _, c) => c);
+
+            foreach(var item in Sensors)
+            {
+                Grid.Push('S', item.Position.X, item.Position.Y);
+                Grid.Push('B', item.ClosestBeacon.X, item.ClosestBeacon.Y);
+            }
+        }
+
+        public List<Sensor> Sensors { get; }
+        public TwoDimensionalGrid<char, int> Grid { get; }
+
+        public string GetAnswerForPart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAnswerForPart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Represents a two-dimensional grid of <see cref="TCell"/>.  Efficiently stores sparse data.
+    /// Thread safe for reading but not while writing.
+    /// </summary>
+    public class TwoDimensionalGrid<TCell, TDimension> where TDimension : notnull, INumber<TDimension>
+    {
+
+        public TwoDimensionalGrid(TCell Empty, Func<TDimension, TDimension, TCell, char> Renderer)
+        {
+            this.Empty = Empty;
+            render = Renderer;
+            CellsByX = new SortedList<TDimension, SortedList<TDimension, TCell>>();
+        }
+
+        /// <summary>
+        /// Pushes <paramref name="value"/> to <paramref name="X"/>, <paramref name="Y"/>.
+        /// Will overwrite existing value if there is one at that coordinate.  Not thread safe.
+        /// </summary>
+        public void Push(TCell value, TDimension X, TDimension Y)
+        {
+            if (CellsByX.TryGetValue(X, out var byY))
+            {
+                if (!byY.TryAdd(Y, value))
+                {
+                    byY[Y] = value;
+                }
+            }
+            else
+            {
+                CellsByX[X] = new SortedList<TDimension, TCell> { { Y, value } };
+            }
+        }
+
+        public TCell Get(TDimension X, TDimension Y)
+        {
+            if (CellsByX.TryGetValue(X, out var byY))
+            {
+                if (byY.TryGetValue(Y, out var value))
+                {
+                    return value;
+                }
+            }
+            return Empty;
+        }
+
+        public TDimension MaxX => CellsByX.LastOrDefault().Key;
+        public TDimension MinX => CellsByX.FirstOrDefault().Key;
+        public TDimension MaxY => CellsByX.Select(c => c.Value.LastOrDefault().Key).DefaultIfEmpty().Max()!;
+        public TDimension MinY => CellsByX.Select(c => c.Value.FirstOrDefault().Key).DefaultIfEmpty().Min()!;
+
+        public (TDimension MinX, TDimension MaxX, TDimension MinY, TDimension MaxY) GetDimensionalLimits()
+            => (MinX, MaxX, MinY, MaxY);
+
+        public string RenderToString(string lineSeparator = "\n")
+        {
+            if (!TDimension.IsInteger(default!))
+            {
+                throw new NotSupportedException($"{nameof(RenderToString)} is currently not supported except on integers.");
+            }
+
+            var limits = GetDimensionalLimits();
+
+            var sb = new StringBuilder();
+
+            for (var y = limits.MinY; y <= limits.MaxY; y++)
+            {
+                for (var x = limits.MinX; x <= limits.MaxX; x++)
+                {
+                    sb.Append(render(x, y, Get(x, y)));
+                }
+                if (y < limits.MaxY)
+                {
+                    sb.Append(lineSeparator);
+                }
+            }
+            return sb.ToString();
+        }
+
+        private readonly Func<TDimension, TDimension, TCell, char> render;
+
+        public TCell Empty { get; init; }
+
+        /// <summary>
+        /// TODO: expose this in a readonly manner.
+        /// </summary>
+        public SortedList<TDimension, SortedList<TDimension, TCell>> CellsByX { get; }
+    }
+
+    public sealed partial record Sensor
+    {
+        public required Point2d Position { get; init; }
+        public required Point2d ClosestBeacon {  get; init; }
+
+        [GeneratedRegex(@"^Sensor at x=(?<PositionX>[+-]?\d*), y=(?<PositionY>[+-]?\d*): closest beacon is at x=(?<ClosestBeaconX>[+-]?\d*), y=(?<ClosestBeaconY>[+-]?\d*)$")]
+        private static partial Regex SensorRegex();
+
+        public static Sensor? MaybeParse(string s)
+        {
+            var match = SensorRegex().Match(s);
+            if (match.Success)
+            {
+                return new Sensor
+                {
+                    Position = new()
+                    {
+                        X = int.Parse(match.Groups["PositionX"].ValueSpan),
+                        Y = int.Parse(match.Groups["PositionY"].ValueSpan)
+                    },
+                    ClosestBeacon = new()
+                    {
+                        X = int.Parse(match.Groups["ClosestBeaconX"].ValueSpan),
+                        Y = int.Parse(match.Groups["ClosestBeaconY"].ValueSpan)
+                    }
+                };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ tests/Day15Tests.cs
@@ -0,0 +1,140 @@
+﻿using day15;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day15Tests
+    {
+        [Fact]
+        public void ParsingWorksAsExpected()
+        {
+            var sut = new Day15(exampleInput1.Split('\n'));
+            sut.Sensors.Count.Should().Be(14);
+
+            sut.Sensors[0].Position.X.Should().Be(2);
+            sut.Sensors[0].Position.Y.Should().Be(18);
+            sut.Sensors[0].ClosestBeacon.X.Should().Be(-2);
+            sut.Sensors[0].ClosestBeacon.Y.Should().Be(15);
+
+            sut.Sensors[13].Position.X.Should().Be(20);
+            sut.Sensors[13].Position.Y.Should().Be(1);
+            sut.Sensors[13].ClosestBeacon.X.Should().Be(15);
+            sut.Sensors[13].ClosestBeacon.Y.Should().Be(3);
+
+            sut.Grid.MaxX.Should().Be(25);
+            sut.Grid.MinX.Should().Be(-2);
+            sut.Grid.MaxY.Should().Be(22);
+            sut.Grid.MinY.Should().Be(0);
+        }
+
+        [Fact]
+        public void EmptyTwoDimensionalGridSupportsMaxAndMinFunctions()
+        {
+            var sut = new TwoDimensionalGrid<char, int>(' ', (_, _, c) => c);
+            sut.MaxX.Should().Be(0);
+            sut.MinX.Should().Be(0);
+            sut.MaxY.Should().Be(0);
+            sut.MinY.Should().Be(0);
+        }
+
+        [Fact]
+        public void TwoDimensionalGridSupportsAddingAndGettingSuccessfully()
+        {
+            var sut = new TwoDimensionalGrid<char, int>(' ', (_, _, c) => c);
+
+            sut.Push('o', 1, 1);
+            sut.Push('*', 1, 2);
+            sut.Push('+', 2, 2);
+            sut.Push('#', -1, -1);
+            sut.Push('@', -1, -1);
+
+            sut.CellsByX.Should().HaveCount(3);
+
+            sut.Get(1, 1).Should().Be('o');
+            sut.Get(1, 2).Should().Be('*');
+            sut.Get(2, 2).Should().Be('+');
+            sut.Get(-1, -1).Should().Be('@');
+            sut.Get(0, 0).Should().Be(' ');
+        }
+
+        [Fact]
+        public void RenderingTwoDimensionalGridToStringWithRendererWorksAsExpected()
+        {
+            var sut = new TwoDimensionalGrid<char, int>('.', RenderOriginAsPercent);
+
+            sut.Push('o', 1, 1);
+            sut.Push('*', 1, 2);
+            sut.Push('+', 2, 2);
+            sut.Push('#', -1, -1);
+            sut.Push('@', -1, -1);
+
+            var result = sut.RenderToString();
+            result.Should().Be("""
+                @...
+                .%..
+                ..o.
+                ..*+
+                """.ReplaceLineEndings("\n"));
+
+            static char RenderOriginAsPercent(int x, int y, char c)
+            {
+                if (x == 0 && y == 0)
+                {
+                    return '%';
+                }
+                return c;
+            }
+        }
+
+        [Fact]
+        public void RenderingTwoDimensionalGridWithExampleDataToStringWorksAsExpected()
+        {
+            var sut = new Day15(exampleInput1.Split('\n'));
+            var result = sut.Grid.RenderToString();
+            result.Should().Be("""
+                ....S.......................
+                ......................S.....
+                ...............S............
+                ................SB..........
+                ............................
+                ............................
+                ............................
+                ..........S.......S.........
+                ............................
+                ............................
+                ....B.......................
+                ..S.........................
+                ............................
+                ............................
+                ..............S.......S.....
+                B...........................
+                ...........SB...............
+                ................S..........B
+                ....S.......................
+                ............................
+                ............S......S........
+                ............................
+                .......................B....
+                """.ReplaceLineEndings("\n"));
+        }
+
+
+        public static readonly string exampleInput1 = """
+            Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+            Sensor at x=9, y=16: closest beacon is at x=10, y=16
+            Sensor at x=13, y=2: closest beacon is at x=15, y=3
+            Sensor at x=12, y=14: closest beacon is at x=10, y=16
+            Sensor at x=10, y=20: closest beacon is at x=10, y=16
+            Sensor at x=14, y=17: closest beacon is at x=10, y=16
+            Sensor at x=8, y=7: closest beacon is at x=2, y=10
+            Sensor at x=2, y=0: closest beacon is at x=2, y=10
+            Sensor at x=0, y=11: closest beacon is at x=2, y=10
+            Sensor at x=20, y=14: closest beacon is at x=25, y=17
+            Sensor at x=17, y=20: closest beacon is at x=21, y=22
+            Sensor at x=16, y=7: closest beacon is at x=15, y=3
+            Sensor at x=14, y=3: closest beacon is at x=15, y=3
+            Sensor at x=20, y=1: closest beacon is at x=15, y=3
+            """.ReplaceLineEndings("\n");
+    }
+}
